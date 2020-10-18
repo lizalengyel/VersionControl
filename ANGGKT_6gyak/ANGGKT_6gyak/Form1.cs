e0b55ca60a0641cb852322c6390f8ee87961a331
Liza Lengyel <liza.lengyel2@stud.uni-corvinus.hu>
2020-10-18 21:26:55 +0200

    xml fv

--- ANGGKT_6gyak/ANGGKT_6gyak/Form1.cs
+++ ANGGKT_6gyak/ANGGKT_6gyak/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ANGGKT_6gyak.Entities;
+using ANGGKT_6gyak.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ANGGKT_6gyak
 {
     public partial class Form1 : Form
     {
+        BindingList<RateDate> Rates = new BindingList<RateDate>();
+
+        public void xmlfunction()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new RateDate();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
+
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            xmlfunction();
+
+           
         }
+
+       
+
     }
 }

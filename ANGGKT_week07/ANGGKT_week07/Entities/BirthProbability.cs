4f438a2f8d29de552d4860f18f66f98134cdb604
Liza Lengyel <liza.lengyel2@stud.uni-corvinus.hu>
2020-10-25 17:53:41 +0100

    3.

---
+++ ANGGKT_week07/ANGGKT_week07/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANGGKT_week07.Entities
+{
+    class BirthProbability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+
+        public double P { get; set; }
+    }
+}
---
+++ ANGGKT_week07/ANGGKT_week07/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANGGKT_week07.Entities
+{
+    class DeathProbability
+    {
+  
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double Deathprobability { get; set; }
+    }
+}

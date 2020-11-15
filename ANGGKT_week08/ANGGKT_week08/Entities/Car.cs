ebbe1462f8f8a45ef5af45d5f536f483990ed304
Liza Lengyel <liza.lengyel2@stud.uni-corvinus.hu>
2020-11-15 20:59:38 +0100

    7.

---
+++ ANGGKT_week08/ANGGKT_week08/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using ANGGKT_week08.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANGGKT_week08.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+
+    }
+}

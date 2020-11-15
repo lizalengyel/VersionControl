933b78b3deb5fec33940444e05275ff02ab65743
Liza Lengyel <liza.lengyel2@stud.uni-corvinus.hu>
2020-11-15 21:02:45 +0100

    9.

--- ANGGKT_week08/ANGGKT_week08/Entities/BallFactory.cs
+++ ANGGKT_week08/ANGGKT_week08/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ANGGKT_week08.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace ANGGKT_week08.Entities
 {
-   public class BallFactory
+   public class BallFactory : IToyFactory
     {
-        public Ball CreateNew()
+        public Toy CreateNew()
         {
             return new Ball();
         }
